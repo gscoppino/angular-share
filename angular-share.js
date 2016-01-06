2e42c11ffbd3037e81f8d7f5be6532c9218efe3c
Giuseppe Scoppino <Giuseppe.Scoppino@gtri.gatech.edu>
2016-01-06 11:39:22 -0500

    Clean up spacing quirks in angular-share.js that were caused by initial code being written in an unconfigured editor.

--- angular-share.js
+++ angular-share.js
@@ -1,186 +1,185 @@
 function shareOptionsController() {
 
-  if (!angular.isObject(this.model) || !this.field || !angular.isArray(this.options)) { return; }
+    if (!angular.isObject(this.model) || !this.field || !angular.isArray(this.options)) { return; }
 
-  /**************/
+    /**************/
 
-  var ctrl = this;
+    var ctrl = this;
 
-  // Configure our scope.
-  ctrl.currentSettings = undefined; // text overview of current sharing options.
+    // Configure our scope.
+    ctrl.currentSettings = undefined; // text overview of current sharing options.
 
-  ctrl.toggleOptions = undefined; // all share options that are boolean flags by nature.
-  ctrl.customOptions = undefined; // all share options that are collections of entities by nature.
+    ctrl.toggleOptions = undefined; // all share options that are boolean flags by nature.
+    ctrl.customOptions = undefined; // all share options that are collections of entities by nature.
 
-  ctrl.active_options_count = 0; // counter to keep track of how many options are set.
-  ctrl.active_toggle_options = []; // array of string for currently active toggle options.
+    ctrl.active_options_count = 0; // counter to keep track of how many options are set.
+    ctrl.active_toggle_options = []; // array of string for currently active toggle options.
 
-  ctrl.has_custom_option = undefined; // flag to indicate that custom options are in use.
+    ctrl.has_custom_option = undefined; // flag to indicate that custom options are in use.
 
-  // Initialize.
-  function initialize() {
-    var toggleOptions = [], customOptions = [];
-    ctrl.model[ctrl.field] = angular.isObject(ctrl.model[ctrl.field]) ? ctrl.model[ctrl.field] : {};
+    // Initialize.
+    function initialize() {
+        var toggleOptions = [], customOptions = [];
+        ctrl.model[ctrl.field] = angular.isObject(ctrl.model[ctrl.field]) ? ctrl.model[ctrl.field] : {};
 
-    ctrl.currentSettings = "Loading...";
-    angular.forEach(ctrl.options, function (shareOption) {
-      if (!shareOption.key) { return; }
+        ctrl.currentSettings = "Loading...";
+        angular.forEach(ctrl.options, function (shareOption) {
+            if (!shareOption.key) { return; }
 
-      ctrl.model[ctrl.field][shareOption.key] = ctrl.model[ctrl.field][shareOption.key] || shareOption.value;
+            ctrl.model[ctrl.field][shareOption.key] = ctrl.model[ctrl.field][shareOption.key] || shareOption.value;
 
-      if (shareOption.type === 'boolean') {
-        toggleOptions.push(shareOption);
+            if (shareOption.type === 'boolean') {
+                toggleOptions.push(shareOption);
 
-	if (shareOption.value === true) {
-	  ctrl.active_options_count++;
-	  ctrl.active_toggle_options.push(shareOption.label);
-	}
+                if (shareOption.value === true) {
+                    ctrl.active_options_count++;
+                    ctrl.active_toggle_options.push(shareOption.label);
+                }
 
-      } else if (shareOption.type === 'collection') {
-        customOptions.push(angular.copy(shareOption));
+            } else if (shareOption.type === 'collection') {
+                customOptions.push(angular.copy(shareOption));
 
-	if (shareOption.value.length) {
-          ctrl.active_options_count++;
+                if (shareOption.value.length) {
+                    ctrl.active_options_count++;
 
-	  if (!ctrl.has_custom_option) {
-	    ctrl.has_custom_option = true;
-	  }
-	}
+                    if (!ctrl.has_custom_option) {
+                        ctrl.has_custom_option = true;
+                    }
+                }
+            }
+        });
 
-      }
-    });
-
-    ctrl.toggleOptions = toggleOptions;
-    ctrl.customOptions = customOptions;
-    ctrl.updateCurrentSettings();
-  }
-
-  // Controller API
-  ctrl.updateCurrentSettings = function () {
-    if (ctrl.has_custom_option) {
-      ctrl.currentSettings = "Custom";
-      return;
+        ctrl.toggleOptions = toggleOptions;
+        ctrl.customOptions = customOptions;
+        ctrl.updateCurrentSettings();
     }
 
-    if (ctrl.active_options_count === 0) {
-      ctrl.currentSettings = "Only Me";
-      return;
-    }
+    // Controller API
+    ctrl.updateCurrentSettings = function () {
+        if (ctrl.has_custom_option) {
+            ctrl.currentSettings = "Custom";
+            return;
+        }
 
-    ctrl.currentSettings = ctrl.active_toggle_options.join(', ');
-  };
+        if (ctrl.active_options_count === 0) {
+            ctrl.currentSettings = "Only Me";
+            return;
+        }
 
-  ctrl.toggleOption = function (shareOption) {
-    ctrl.model[ctrl.field][shareOption.key] = !shareOption.value; // Update the model
-    shareOption.value = !shareOption.value; // Update the view
+        ctrl.currentSettings = ctrl.active_toggle_options.join(', ');
+    };
 
-    if (shareOption.value === true) {
+    ctrl.toggleOption = function (shareOption) {
+        ctrl.model[ctrl.field][shareOption.key] = !shareOption.value; // Update the model
+        shareOption.value = !shareOption.value; // Update the view
 
-      ctrl.active_options_count++;
-      ctrl.active_toggle_options.push(shareOption.label);
+        if (shareOption.value === true) {
 
-    } else {
+            ctrl.active_options_count++;
+            ctrl.active_toggle_options.push(shareOption.label);
 
-      ctrl.active_options_count--;
-      ctrl.active_toggle_options.splice(ctrl.active_toggle_options.indexOf(shareOption.label), 1);
+        } else {
 
-    }
+            ctrl.active_options_count--;
+            ctrl.active_toggle_options.splice(ctrl.active_toggle_options.indexOf(shareOption.label), 1);
 
-    ctrl.updateCurrentSettings();
-  };
+        }
 
-  ctrl.clearToggleOptions = function () {
-    angular.forEach(ctrl.toggleOptions, function (shareOption) {
-      ctrl.model[ctrl.field][shareOption.key] = false; // Update the model
-      if (shareOption.value === true) {
-        shareOption.value = false; // Update the view
-        ctrl.active_options_count--;
-      }
-    });
+        ctrl.updateCurrentSettings();
+    };
 
-    ctrl.active_toggle_options = [];
-    ctrl.updateCurrentSettings();
-  };
+    ctrl.clearToggleOptions = function () {
+        angular.forEach(ctrl.toggleOptions, function (shareOption) {
+            ctrl.model[ctrl.field][shareOption.key] = false; // Update the model
+            if (shareOption.value === true) {
+                shareOption.value = false; // Update the view
+                ctrl.active_options_count--;
+            }
+        });
 
-  ctrl.clearCustomOptions = function () {
-    angular.forEach(ctrl.customOptions, function (shareOption) {
-      ctrl.model[ctrl.field][shareOption.key] = []; // Update the model
-      if (shareOption.value.length) {
-        shareOption.value = []; // Update the view
-        ctrl.active_options_count--;
-      }
-    });
+        ctrl.active_toggle_options = [];
+        ctrl.updateCurrentSettings();
+    };
 
-    ctrl.has_custom_option = false;
-    ctrl.updateCurrentSettings();
-  };
+    ctrl.clearCustomOptions = function () {
+        angular.forEach(ctrl.customOptions, function (shareOption) {
+            ctrl.model[ctrl.field][shareOption.key] = []; // Update the model
+            if (shareOption.value.length) {
+                shareOption.value = []; // Update the view
+                ctrl.active_options_count--;
+            }
+        });
 
-  ctrl.clearSetOptions = function () {
-    ctrl.clearToggleOptions();
-    ctrl.clearCustomOptions();
-  };
+        ctrl.has_custom_option = false;
+        ctrl.updateCurrentSettings();
+    };
 
-  ctrl.openCustomOptionsModal = function () {
-    var modal = CustomOptionsService.openModal({
-      customOptions: ctrl.customOptions
-    });
+    ctrl.clearSetOptions = function () {
+        ctrl.clearToggleOptions();
+        ctrl.clearCustomOptions();
+    };
 
-    modal.result.then(function (customOptions) {
-      angular.forEach(customOptions, function (shareOption) {
-        // Diff the old custom options with the new ones
-        // to update active options counter.
+    ctrl.openCustomOptionsModal = function () {
+        var modal = CustomOptionsService.openModal({
+            customOptions: ctrl.customOptions
+        });
 
-        // Update the original model.
+        modal.result.then(function (customOptions) {
+            angular.forEach(customOptions, function (shareOption) {
+                // Diff the old custom options with the new ones
+                // to update active options counter.
 
-        // Set the new custom options on the controller
-      });
-    });
-  };
+                // Update the original model.
 
-  // Initialize
-  initialize();
+                // Set the new custom options on the controller
+            });
+        });
+    };
+
+    // Initialize
+    initialize();
 }
 
 angular.module('angular-share', ['ui.bootstrap'])
-  .directive('sharingOptions', function () {
-    return {
-      scope: {},
-      template: '\
-        <div uib-dropdown auto-close="outsideClick">\
-	  <button type="button" class="btn btn-default" uib-dropdown-toggle>\
-	    {{ ctrl.currentSettings }}\
-	    <span class="caret"></span>\
-	  </button>\
-	  <ul class="uib-dropdown-menu">\
-      <li ng-click="ctrl.clearSetOptions()">\
-        <a href="#">\
-          <span class="fa" ng-class="{\'fa-check-square-o\': ctrl.active_options_count === 0, \'fa-square-o\': ctrl.active_options_count > 0}"></span>\
-          <span>Only Me</span>\
-        </a>\
-      </li>\
-      <li ng-if="ctrl.toggleOptions.length" class="divider"></li>\
-	    <li ng-repeat="option in ::ctrl.toggleOptions" ng-click="ctrl.toggleOption(option)">\
-              <a href="#">\
-                <span class="fa" ng-class="{\'fa-check-square-o\': option.value, \'fa-square-o\': !option.value}"></span>\
-                <span>{{ option.label }}</span>\
-              </a>\
-	    </li>\
-	    <li ng-if="ctrl.customOptions.length" class="divider"></li>\
-	    <li ng-if="ctrl.customOptions.length">\
-	      <a href="#">\
-          <span class="fa fa-cog"></span>\
-          <span>Custom</span>\
-        </a>\
-	    </li>\
-	  </ul>\
-	</div>\
-      ',
-      controller: shareOptionsController,
-      controllerAs: 'ctrl',
-      bindToController: {
-        model: '=',
-	field: '@',
-	options: '='
-      }
-    };
-  });
+    .directive('sharingOptions', function () {
+        return {
+            scope: {},
+            template: '\
+                <div uib-dropdown auto-close="outsideClick">\
+                  <button type="button" class="btn btn-default" uib-dropdown-toggle>\
+                    <span>{{ ctrl.currentSettings }}</span>\
+                    <span class="caret"></span>\
+                  </button>\
+                  <ul class="uib-dropdown-menu">\
+                    <li ng-click="ctrl.clearSetOptions()">\
+                      <a href="#">\
+                        <span class="fa" ng-class="{\'fa-check-square-o\': ctrl.active_options_count === 0, \'fa-square-o\': ctrl.active_options_count > 0}"></span>\
+                        <span>Only Me</span>\
+                      </a>\
+                    </li>\
+                    <li ng-if="ctrl.toggleOptions.length" class="divider"></li>\
+                    <li ng-repeat="option in ::ctrl.toggleOptions" ng-click="ctrl.toggleOption(option)">\
+                      <a href="#">\
+                        <span class="fa" ng-class="{\'fa-check-square-o\': option.value, \'fa-square-o\': !option.value}"></span>\
+                        <span>{{ option.label }}</span>\
+                      </a>\
+                    </li>\
+                    <li ng-if="ctrl.customOptions.length" class="divider"></li>\
+                    <li ng-if="ctrl.customOptions.length">\
+                      <a href="#">\
+                        <span class="fa fa-cog"></span>\
+                        <span>Custom</span>\
+                      </a>\
+                    </li>\
+                  </ul>\
+                </div>\
+            ',
+            controller: shareOptionsController,
+            controllerAs: 'ctrl',
+            bindToController: {
+                model: '=',
+                field: '@',
+                options: '='
+            }
+        };
+    });
