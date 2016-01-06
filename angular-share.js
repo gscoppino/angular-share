4e8669349e46a4c3bb7ad2ccee855407479a0f3a
Giuseppe Scoppino <Giuseppe.Scoppino@gtri.gatech.edu>
2016-01-06 11:34:19 -0500

    Replace the word 'permissions' with 'options'. Comments to describe future implementation of custom options.

--- angular-share.js
+++ angular-share.js
@@ -8,12 +8,16 @@ function shareOptionsController() {
 
   // Configure our scope.
   ctrl.currentSettings = undefined; // text overview of current sharing options.
+
   ctrl.toggleOptions = undefined; // all share options that are boolean flags by nature.
   ctrl.customOptions = undefined; // all share options that are collections of entities by nature.
-  ctrl.active_permissions_count = 0; // counter to keep track of how many permissions are set.
-  ctrl.active_toggle_permissions = []; // array of string for currently active toggle permissions.
-  ctrl.has_custom_permission = undefined; // flag to indicate that custom permissions are in use.
 
+  ctrl.active_options_count = 0; // counter to keep track of how many options are set.
+  ctrl.active_toggle_options = []; // array of string for currently active toggle options.
+
+  ctrl.has_custom_option = undefined; // flag to indicate that custom options are in use.
+
+  // Initialize.
   function initialize() {
     var toggleOptions = [], customOptions = [];
     ctrl.model[ctrl.field] = angular.isObject(ctrl.model[ctrl.field]) ? ctrl.model[ctrl.field] : {};
@@ -28,18 +32,18 @@ function shareOptionsController() {
         toggleOptions.push(shareOption);
 
 	if (shareOption.value === true) {
-	  ctrl.active_permissions_count++;
-	  ctrl.active_toggle_permissions.push(shareOption.label);
+	  ctrl.active_options_count++;
+	  ctrl.active_toggle_options.push(shareOption.label);
 	}
 
       } else if (shareOption.type === 'collection') {
         customOptions.push(angular.copy(shareOption));
 
 	if (shareOption.value.length) {
-          ctrl.active_permissions_count++;
+          ctrl.active_options_count++;
 
-	  if (!ctrl.has_custom_permission) {
-	    ctrl.has_custom_permission = true;
+	  if (!ctrl.has_custom_option) {
+	    ctrl.has_custom_option = true;
 	  }
 	}
 
@@ -53,17 +57,17 @@ function shareOptionsController() {
 
   // Controller API
   ctrl.updateCurrentSettings = function () {
-    if (ctrl.has_custom_permission) {
+    if (ctrl.has_custom_option) {
       ctrl.currentSettings = "Custom";
       return;
     }
 
-    if (ctrl.active_permissions_count === 0) {
+    if (ctrl.active_options_count === 0) {
       ctrl.currentSettings = "Only Me";
       return;
     }
 
-    ctrl.currentSettings = ctrl.active_toggle_permissions.join(', ');
+    ctrl.currentSettings = ctrl.active_toggle_options.join(', ');
   };
 
   ctrl.toggleOption = function (shareOption) {
@@ -72,13 +76,13 @@ function shareOptionsController() {
 
     if (shareOption.value === true) {
 
-      ctrl.active_permissions_count++;
-      ctrl.active_toggle_permissions.push(shareOption.label);
+      ctrl.active_options_count++;
+      ctrl.active_toggle_options.push(shareOption.label);
 
     } else {
 
-      ctrl.active_permissions_count--;
-      ctrl.active_toggle_permissions.splice(ctrl.active_toggle_permissions.indexOf(shareOption.label), 1);
+      ctrl.active_options_count--;
+      ctrl.active_toggle_options.splice(ctrl.active_toggle_options.indexOf(shareOption.label), 1);
 
     }
 
@@ -90,11 +94,11 @@ function shareOptionsController() {
       ctrl.model[ctrl.field][shareOption.key] = false; // Update the model
       if (shareOption.value === true) {
         shareOption.value = false; // Update the view
-        ctrl.active_permissions_count--;
+        ctrl.active_options_count--;
       }
     });
 
-    ctrl.active_toggle_permissions = [];
+    ctrl.active_toggle_options = [];
     ctrl.updateCurrentSettings();
   };
 
@@ -103,17 +107,34 @@ function shareOptionsController() {
       ctrl.model[ctrl.field][shareOption.key] = []; // Update the model
       if (shareOption.value.length) {
         shareOption.value = []; // Update the view
-        ctrl.active_permissions_count--;
+        ctrl.active_options_count--;
       }
     });
 
-    ctrl.has_custom_permission = false;
+    ctrl.has_custom_option = false;
     ctrl.updateCurrentSettings();
   };
 
   ctrl.clearSetOptions = function () {
     ctrl.clearToggleOptions();
     ctrl.clearCustomOptions();
+  };
+
+  ctrl.openCustomOptionsModal = function () {
+    var modal = CustomOptionsService.openModal({
+      customOptions: ctrl.customOptions
+    });
+
+    modal.result.then(function (customOptions) {
+      angular.forEach(customOptions, function (shareOption) {
+        // Diff the old custom options with the new ones
+        // to update active options counter.
+
+        // Update the original model.
+
+        // Set the new custom options on the controller
+      });
+    });
   };
 
   // Initialize
@@ -133,7 +154,7 @@ angular.module('angular-share', ['ui.bootstrap'])
 	  <ul class="uib-dropdown-menu">\
       <li ng-click="ctrl.clearSetOptions()">\
         <a href="#">\
-          <span class="fa" ng-class="{\'fa-check-square-o\': ctrl.active_permissions_count === 0, \'fa-square-o\': ctrl.active_permissions_count > 0}"></span>\
+          <span class="fa" ng-class="{\'fa-check-square-o\': ctrl.active_options_count === 0, \'fa-square-o\': ctrl.active_options_count > 0}"></span>\
           <span>Only Me</span>\
         </a>\
       </li>\
