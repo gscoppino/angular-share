8c8a492b744f3a1c1356daa5cc1f65adbdd50f3d
Giuseppe Scoppino <Giuseppe.Scoppino@gtri.gatech.edu>
2016-01-06 16:05:14 -0500

    Serve demo using gulp. Give the demo its own separate package.json.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 (function () {
     var gulp = require('gulp');
+    var connect = require('gulp-connect');
 
     gulp.task('build', function () {
         return gulp.src(['src/*.js'])
-            .pipe(gulp.dest('dist'));
+            .pipe(gulp.dest('dist'))
+            .pipe(gulp.dest('demo/vendor'))
+            .pipe(connect.reload());
     });
 
-    gulp.task('default', ['build']);
+    gulp.task('serve', ['build'], function () {
+        connect.server({
+            root: 'demo',
+            livereload: true
+        });
+    });
+
+    gulp.task('default', ['build', 'serve']);
 }());
