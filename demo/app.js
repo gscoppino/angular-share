483a19a96ed9937fbd02a10af7890e0d0221f782
Giuseppe Scoppino <Giuseppe.Scoppino@gtri.gatech.edu>
2016-01-06 16:12:00 -0500

    Clean up spacing quirks in 'demo/app.js' that were caused by initial code being written in an unconfigured editor.

--- demo/app.js
+++ demo/app.js
@@ -1,49 +1,49 @@
 angular.module('test', ['angular-share', 'ngMaterial'])
-  .directive('test', function () {
-    return {
-      scope: {},
-      template: '<sharing-options model="testCtrl.resourceModel" field="permissions" options="::testCtrl.shareOptions"></sharing-options>',
-      controller: function ($scope) {
-        var ctrl = this;
+    .directive('test', function () {
+        return {
+            scope: {},
+            template: '<sharing-options model="testCtrl.resourceModel" field="permissions" options="::testCtrl.shareOptions"></sharing-options>',
+            controller: function ($scope) {
+                var ctrl = this;
 
-        ctrl.resourceModel = {
-	  id: 1,
-	  name: 'Test Resource Model'
-	};
+                ctrl.resourceModel = {
+                    id: 1,
+                    name: 'Test Resource Model'
+                };
 
-	$scope.$watch(function () {
-	  return ctrl.resourceModel;
-	}, function (newVal, oldVal) {
-	  console.log("New Model Value: ", newVal);
-	}, true);
+                $scope.$watch(function () {
+                  return ctrl.resourceModel;
+                }, function (newVal, oldVal) {
+                  console.log("New Model Value: ", newVal);
+                }, true);
 
-	ctrl.shareOptions = [
-	  {
-	    key: 'family',
-	    label: 'Family',
-	    type: 'boolean',
-	    value: false
-	  },
-	  {
-	    key: 'staff',
-	    label: 'Staff',
-	    type: 'boolean',
-	    value: false
-	  },
-	  {
-	    key: 'users',
-	    label: 'Users',
-	    type: 'collection',
-	    value: [1]
-	  },
-	  {
-	    key: 'groups',
-	    label: 'Groups',
-	    type: 'collection',
-	    value: [1, 2]
-	  }
-	];
-      },
-      controllerAs: 'testCtrl',
-    };
-  });
+                ctrl.shareOptions = [
+                    {
+                        key: 'family',
+                        label: 'Family',
+                        type: 'boolean',
+                        value: false
+                    },
+                    {
+                        key: 'staff',
+                        label: 'Staff',
+                        type: 'boolean',
+                        value: false
+                    },
+                    {
+                        key: 'users',
+                        label: 'Users',
+                        type: 'collection',
+                        value: [1]
+                    },
+                    {
+                        key: 'groups',
+                        label: 'Groups',
+                        type: 'collection',
+                        value: [1, 2]
+                    }
+                ];
+            },
+            controllerAs: 'testCtrl',
+        };
+    });
