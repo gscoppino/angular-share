23fdcc04bebb51eab6c74a9de053a8f448333c0e
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-01-07 22:00:20 -0500

    dev: added some crude watchers to the gulpfile, for livereload on changes to angular-share or the demo files.

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,26 @@
     gulp.task('build', function () {
         return gulp.src(['src/*.js'])
             .pipe(gulp.dest('dist'))
-            .pipe(gulp.dest('demo/vendor'))
-            .pipe(connect.reload());
+            .pipe(gulp.dest('demo/vendor'));
     });
 
-    gulp.task('serve', ['build'], function () {
+    gulp.task('build:watch', ['build'], function () {
         connect.server({
             root: 'demo',
             livereload: true
         });
+
+        gulp.watch('src/*.js', ['build'])
+            .on('change', function (event) {
+                console.log('File' + event.path + ' was ' + event.type + '.');
+                return gulp.src('src/*.js').pipe(connect.reload());
+            });
+
+        gulp.watch('demo/*', function (event) {
+            console.log('File' + event.path + ' was ' + event.type + '.');
+            return gulp.src('demo/*').pipe(connect.reload());
+        });
     });
 
-    gulp.task('default', ['build', 'serve']);
+    gulp.task('default', ['build:watch']);
 }());
